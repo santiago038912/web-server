4ba2d0df8f3c44616106aab08ff446cb37dfa4a6
santiago038912 <santiago038912@gmail.com>
2022-08-29 19:52:02 -0500

    Leemos de un archivo json, lo parseamos a una estructura y lo mandamos por el metodo GET

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"github.com/gin-gonic/gin"
+)
+
+type Producto struct {
+	Id int
+	Nombre string
+	Precio int
+	Stock int
+	Codigo string
+	Publicado bool
+	FechaDeCreacion string
+}
 
 func main() {
+
+	var p []Producto
+
+	jsonFile, err := os.ReadFile("productos.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err2 := json.Unmarshal([]byte(jsonFile), &p)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+
 	// Crea un router con Gin
 	router := gin.Default()
 
 	// Captura la solicitud GET de /hello-world
-	router.GET("/hello-world", func(c *gin.Context) {
+	router.GET("/productos", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World!!",
+			"Producto": p,
 		})
 	})
 	
